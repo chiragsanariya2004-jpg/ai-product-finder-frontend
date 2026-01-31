f9eb87a4458c1e98df5f59532686e0c740d1ccd7
chiragsanariya2004-jpg <chiragsanariya2004@gmail.com>
2026-01-31 20:11:17 +0530

    add seed route

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def find_product(
  ):
 
  @app.post("/seed")
-def seed_products(db: Session = Depends(get_db)):
+ def seed_products(db: Session = Depends(get_db)):
     p1 = Product(name="HP Laptop", price=48000, rating=4.3, best_for="Students")
     p2 = Product(name="Dell Laptop", price=52000, rating=4.4, best_for="Office")
     p3 = Product(name="Lenovo Laptop", price=45000, rating=4.2, best_for="Coding")
